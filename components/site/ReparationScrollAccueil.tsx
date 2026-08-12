b39bf68e3b29eecbb1bf17b682ce7b65b1af505b
Ton Nom <lopes.jerome21@gmail.com>
2026-08-13 00:21:50 +0200

    Libere le scroll sous la popup accueil

--- components/site/ReparationScrollAccueil.tsx
+++ components/site/ReparationScrollAccueil.tsx
@@ -9,11 +9,16 @@ export default function ReparationScrollAccueil() {
       window.requestAnimationFrame(reparerScrollSansPanneau);
     }
 
+    const observateur = new MutationObserver(reparer);
+    observateur.observe(document.documentElement, { attributes: true, attributeFilter: ['class', 'style'] });
+    observateur.observe(document.body, { attributes: true, attributeFilter: ['class', 'style'] });
+
     reparer();
     window.addEventListener('pageshow', reparer);
     document.addEventListener('visibilitychange', reparer);
 
     return () => {
+      observateur.disconnect();
       window.removeEventListener('pageshow', reparer);
       document.removeEventListener('visibilitychange', reparer);
     };
